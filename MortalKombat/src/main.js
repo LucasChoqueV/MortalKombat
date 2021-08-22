a2523b61377a85b44e97f2d19b0ea44109d269ad
Lucas Choque <lchoque@k27.com.ar>
2021-08-22 19:43:11 -0300

    Initial structure

---
+++ MortalKombat/src/main.js
@@ -0,0 +1,29 @@
+import Bootloader from './Bootloader.js';
+
+const config = {
+    title: "MortalKombat",
+    version: "0.0.1",
+    type: Phaser.AUTO,
+    scale: {
+        parent: "phaser_container",
+        width: 800,
+        height: 600,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    backgroundColor: "#f9ca24",
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        "arcade": {
+            gravity: {
+                y: 500
+            }
+        }
+    },
+    scene: [
+        Bootloader
+    ]
+};
+
+new Phaser.Game(config);
